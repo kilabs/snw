4432d7ce8b4410ac9dab254b08745a79b6a4ae91
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-09-15 08:29:00 +0700

    update form signup

--- js/agency.js
+++ js/agency.js
@@ -41,7 +41,7 @@ $(function(){
 			$(this).addClass("open");
 			$(".form-popup").stop(true,true).animate({
 				width:350,
-				height:420,
+				height:520,
 			},300).css("padding","15px 20px");                
 		}            
 	});
